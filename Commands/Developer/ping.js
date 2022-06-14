44a8ac37b8ac15788f4f91a05c35a72cb43e3f21
dionannd <dionananda77@gmail.com>
2022-06-14 21:21:49 +0700

    fix: join-to-create not work & disable command handler

---
+++ Commands/Developer/ping.js
@@ -0,0 +1,15 @@
+const { CommandInteraction } = require("discord.js");
+
+module.exports = {
+  name: "ping",
+  description: "Ping",
+  permission: "ADMINISTRATOR",
+
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   */
+  execute(interaction) {
+    interaction.reply({ content: "Pong!" });
+  },
+};
